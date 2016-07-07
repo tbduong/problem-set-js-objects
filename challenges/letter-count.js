26430d6d6ce6f95b3cfc72ce376cfd4a185ea1b7
tbduong <tiffaniduong@gmail.com>
2016-07-06 22:05:10 -0700

    set up wordArray and sort wordArray

--- challenges/letter-count.js
+++ challenges/letter-count.js
@@ -29,5 +29,44 @@
     ```
 
 */
-
 // YOUR CODE HERE
+
+// var randStr = {
+//   strCharCounter: function(word){
+//     for(i=0; i < word.length; i++){
+//       var strChecker;
+//       if (word[i]===word[i+1]){
+//         strChecker++;
+//       }
+//       else {
+//         console.log(word[i] + ": ");
+//       }
+//     }
+//   }
+// };
+
+
+// Create an empty object.
+// Loop through the array.
+// While looping, for each of array element, create an object property of it (for that empty object).
+// The value of that object property will then be filled with array of index where the element (object property) is found.
+//
+
+
+
+
+
+//This function will turn the word into an array (wordArray).
+function letterCount(word){
+    var wordArray = word.split("");
+    console.log(wordArray);
+    var sortedWordArray = wordArray.sort();   //sorts array
+    console.log(wordArray);
+}
+  for (var i= wordArray.length-1; i>0; i++);
+    
+
+
+
+
+}
